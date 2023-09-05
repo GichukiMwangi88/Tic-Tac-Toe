8add2038e2af611219225b33463036b984692a4a
Gichuki Mwangi <robertmwangi732@gmail.com>
2023-09-05 16:13:37 -0700

    Updated the font.

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-/*---Begin Game */
+/*----Object that displays the game board*/
+
+const Gameboard = (() => {
+    let gameboard = ["", "", "", "", "","","", "", ""]; //array storing the gameboard
+
+    //Way to render the board on display
+
+    const render = () => {
+        let boardHTML = "";
+    }
+
+
+
+
+
+
+
+
+
+
+
+})();
+
+
+
+
+
+
+
+/*---Click Begin Button and Game board displays */
 
 const begin = document.querySelector("#begin");
 
 begin.addEventListener("click", () => {
-    alert("Hello!")
+    //alert("Hello!")
 });
