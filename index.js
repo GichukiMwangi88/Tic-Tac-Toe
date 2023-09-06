af59f77a60d3ad64d379c82b76962b355ebfc3db
Gichuki Mwangi <robertmwangi732@gmail.com>
2023-09-06 15:59:56 -0700

    Updated the html, css, ensured the game board is centered. Also, when user inputs the players and clicks begin game, grid display 3 X 3 appears on page.

--- index.js
+++ index.js
@@ -10,16 +10,51 @@ const Gameboard = (() => {
     const render = () => {
         let boardHTML = "";
         gameboard.forEach((square, index) => {
-            boardHTML += `<div class="square" id="square-${index}>${square}</div>`
+            boardHTML += `<div class="square" id="square-${index}">${square}</div>`
         })
+
+        // Add the the created board to the DOM element #board
+        document.querySelector(".gameboard").innerHTML = boardHTML;
 
     }
 
-    // Add the the created board to the DOM element #board
-    document.querySelector(".gameboard").innerHTML = boardHTML;
+    
 
     return {
         render
+    }
+
+})();
+
+// Factory Function that creates players
+
+const createPlayer = (name, mark) => {
+    return {
+        name, mark
+    }
+}
+
+/*--- Game Controller- controls when the game starts----*/
+
+const Game = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
+
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector("#playerOne").value, "X"),
+            createPlayer(document.querySelector("#playerTwo").value, "O")
+        ]
+
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render();
+    }
+
+    return {
+        start
     }
 
 })();
@@ -30,7 +65,8 @@ const Gameboard = (() => {
 const beginGame = document.querySelector("#begin");
 
 beginGame.addEventListener("click", () => {
-       alert("Hello!");
+       //alert("Hello!");
+       Game.start();
     
 
 });
