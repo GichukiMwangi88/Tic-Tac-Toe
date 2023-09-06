71ae326193f89b5c04151370e417fc2f82cb436d
Gichuki Mwangi <robertmwangi732@gmail.com>
2023-09-06 14:22:11 -0700

    Updated the html and css layout.

--- index.js
+++ index.js
@@ -4,33 +4,40 @@ const Gameboard = (() => {
     let gameboard = ["", "", "", "", "","","", "", ""]; //array storing the gameboard
 
     //Way to render the board on display
+    // use a loop to loop through each element in 
+    // the array and add it to the game board
 
     const render = () => {
         let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class="square" id="square-${index}>${square}</div>`
+        })
+
     }
 
+    // Add the the created board to the DOM element #board
+    document.querySelector(".gameboard").innerHTML = boardHTML;
 
-
-
-
-
-
-
-
-
+    return {
+        render
+    }
 
 })();
 
 
-
-
-
-
-
 /*---Click Begin Button and Game board displays */
 
-const begin = document.querySelector("#begin");
+const beginGame = document.querySelector("#begin");
 
-begin.addEventListener("click", () => {
-    //alert("Hello!")
+beginGame.addEventListener("click", () => {
+       alert("Hello!");
+    
+
 });
+
+
+
+
+
+
+
