53e3a6e883d22a72e1f4828a92e30ff4a0681b8e
GichukiMwangi88 <robertmwangi732@gmail.com>
2023-09-07 13:25:23 -0700

    Added more logic to the tic tac toe game and validation.

--- index.js
+++ index.js
@@ -1,79 +1,88 @@
 /*----Object that displays the game board*/
 
 const Gameboard = (() => {
-    let gameboard = ["", "", "", "", "","","", "", ""]; //array storing the gameboard
+  let gameboard = ["", "", "", "", "", "", "", "", ""]; //array storing the gameboard
 
-    //Way to render the board on display
-    // use a loop to loop through each element in 
-    // the array and add it to the game board
+  //Way to render the board on display
+  // use a loop to loop through each element in
+  // the array and add it to the game board
 
-    const render = () => {
-        let boardHTML = "";
-        gameboard.forEach((square, index) => {
-            boardHTML += `<div class="square" id="square-${index}">${square}</div>`
-        })
+  const render = () => {
+    let boardHTML = "";
+    gameboard.forEach((square, index) => {
+      //Validation - ensure player names are entered.
+      if (
+        document.querySelector("#playerOne").value === "" &&
+        document.querySelector("#playerTwo").value === ""
+      ) {
+        return;
+      } else {
+        boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
+      }
+    });
 
-        // Add the the created board to the DOM element #board
-        document.querySelector(".gameboard").innerHTML = boardHTML;
+    // Add the the created board to the DOM element #board
+    document.querySelector(".gameboard").innerHTML = boardHTML;
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.addEventListener("click", Game.handleClick);
+    });
+  };
 
-    }
+  const update = (index, value) => {
+    gameboard[index] = value;
+    render();
+  };
 
-    
-
-    return {
-        render
-    }
-
+  return {
+    render,
+    update,
+  };
 })();
 
 // Factory Function that creates players
 
 const createPlayer = (name, mark) => {
-    return {
-        name, mark
-    }
-}
+  return {
+    name,
+    mark,
+  };
+};
 
 /*--- Game Controller- controls when the game starts----*/
 
 const Game = (() => {
-    let players = [];
-    let currentPlayerIndex;
-    let gameOver;
+  let players = [];
+  let currentPlayerIndex;
+  let gameOver;
 
+  const start = () => {
+    players = [
+      createPlayer(document.querySelector("#playerOne").value, "X"),
+      createPlayer(document.querySelector("#playerTwo").value, "O"),
+    ];
 
-    const start = () => {
-        players = [
-            createPlayer(document.querySelector("#playerOne").value, "X"),
-            createPlayer(document.querySelector("#playerTwo").value, "O")
-        ]
+    currentPlayerIndex = 0;
+    gameOver = false;
+    Gameboard.render();
+  };
+  const handleClick = (event) => {
+    let index = parseInt(event.target.id.split("-")[1]);
+    Gameboard.update(index, players[currentPlayerIndex].mark);
 
-        currentPlayerIndex = 0;
-        gameOver = false;
-        Gameboard.render();
-    }
+    currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+  };
 
-    return {
-        start
-    }
-
+  return {
+    start,
+    handleClick,
+  };
 })();
-
 
 /*---Click Begin Button and Game board displays */
 
 const beginGame = document.querySelector("#begin");
 
 beginGame.addEventListener("click", () => {
-       //alert("Hello!");
-       Game.start();
-    
-
+  Game.start();
 });
-
-
-
-
-
-
-
